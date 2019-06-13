a10c07fc42a3ef344ea4a5080514a9e08c3f2ff1
remorses <beats.by.morse@gmail.com>
2019-06-13 16:37:18 +0200

    added colored logs

---
+++ module/__init__.py
@@ -0,0 +1,8 @@
+
+import coloredlogs, logging
+
+logger = logging.getLogger(__name__)
+
+coloredlogs.DEFAULT_FIELD_STYLES = {'asctime': {'color': 'white'}, 'hostname': {'color': 'white'}, 'levelname': {'color': 'white', 'bold': True}, 'name': {'color': 'white'}, 'programname': {'color': 'white'}}
+coloredlogs.install(fmt='%(asctime)s %(levelname)s %(message)s', logger=logger, level='DEBUG')
+
