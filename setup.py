146817bfab909a04251fd76e3339f8cf9d7dd024
remorses <beats.by.morse@gmail.com>
2019-08-24 15:15:14 +0200

    better setup

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-
-
 from setuptools import setup
 
-
+NAME = 'module'
 setup(
-    name='TODO',
+    name=NAME,
     version=open('VERSION').read().strip(),
 
-    description='TODO',
+    description=NAME,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 
@@ -15,19 +13,18 @@ setup(
     author_email='daer.tommy@gmail.com',
     license='Apache Software License 2.0',
 
-    url='https://github.com/remorses/TODO',
+    url=f'https://github.com/remorses/{NAME}',
     keywords=['TODO'],
     install_requires=[x for x in open('./requirements.txt').read().strip().split('\n') if x.strip()],
     package_data={'': ['*.yaml', '*.json', '*.yml']},
     include_package_data=True,
     classifiers=[
-
         'Intended Audience :: Information Technology',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    packages=['module'],
+    packages=[NAME],
 )
 
 
